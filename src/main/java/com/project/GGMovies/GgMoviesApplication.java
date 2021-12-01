4d9f2f61f847fda85db4f51e45a27a06a95ad930
PcStratos <PcStratos@192.168.1.2>
2021-12-01 21:00:53 +0200

    initial commit

---
+++ src/main/java/com/project/GGMovies/GgMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.project.GGMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/GGMovies/GgMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GGMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
